fixed, normalization still needs to be worked out

graph_nn_physics/__main__.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     device = torch.device(params['device'])
 
     network = GraphNetwork(
-        node_dim=15,
+        node_dim=22,
         edge_dim=1,
         global_dim=1,
         mp_steps=params['mp_steps'],
@@ -46,7 +46,7 @@ if __name__ == '__main__':
     criterion = torch.nn.MSELoss()
 
     for epoch, batch in enumerate(tqdm(loader, total=params['epochs'])):
-        if epoch >= params['epochs'] - 1:
+        if epoch >= params['epochs']:
             break
         optimizer.zero_grad()
 

graph_nn_physics/data.py
@@ -62,13 +62,13 @@ class SimulationDataset(Dataset):
         # print(vels.shape)
 
         attrs = self.file[self.group].attrs
-        mean = torch.tensor(attrs['vel_mean'])
-        std = torch.tensor(attrs['vel_std'])
+        # mean = torch.tensor(attrs['vel_mean'])
+        # std = torch.tensor(attrs['vel_std'])
         radius = attrs['default_connectivity_radius']
 
         # normalization
         # print('Pre normalization: {}'.format(vels))
-        vels = torch.div(torch.sub(vels, mean), std)
+        # vels = torch.div(torch.sub(vels, mean), std)
         # print('Post normalization: {}'.format(vels))
 
         # get next acceleration as next vel - last vel given to network

graph_nn_physics/gnn.py
@@ -32,7 +32,7 @@ class GraphNetwork(nn.Module):
             edge_dim += dim + 1
 
         self._edge_encoder = self._construct_mlp(
-            edge_dim, encoder_hidden_dim, encoder_hidden, ee_dim, batch_norm=True)
+            edge_dim, encoder_hidden_dim, encoder_hidden, ee_dim)
 
         # phi_e/phi_v, process edges and nodes into intermediate latent states
         self._edge_processors = []
@@ -176,7 +176,6 @@ class GraphNetwork(nn.Module):
             masked_edges = torch.narrow(graph.edges, 0, 0, graph.n_edges)
 
             # sum receivers for every node
-
             receivers = graph.receivers.unsqueeze(1).repeat(1, self.ve_dim)
             zeros = torch.zeros_like(masked_edges)
             scattered_edge_states = zeros.scatter_add(0, receivers, masked_edges)